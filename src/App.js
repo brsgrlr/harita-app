f4d3c87aef521c2f1d6fd20187f3ff68799baafa
brsgrlr <brsgrlr@hotmail.com>
2022-05-29 16:42:18 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import './App.css';
+
+function App() {
+  return (
+    <MapContainer center={[39.062245, 34.387842]} zoom={18} scrollWheelZoom={true}>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={[39.062245, 34.387842]}>
+        <Popup>
+          Mucur MYO
+        </Popup>
+      </Marker>
+    </MapContainer>
+  );
+}
+
+export default App;
